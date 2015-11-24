00926e283c71de17084e4f0d9260bb123aa74e31
johnss518 <stacy.m.johnson@gmail.com>
2015-11-23 16:30:21 -0800

    blackjack basic implementation

--- BlackJack/FISAppDelegate.m
+++ BlackJack/FISAppDelegate.m
@@ -1,11 +1,17 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISBlackjackGame.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    FISBlackjackGame *game = [[FISBlackjackGame alloc] init];
+    for(NSUInteger i=0; i<5; i++) {
+        [game playBlackjack];
+    }
+    
     return YES;
 }
 
